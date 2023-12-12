7c672781f2ff685aa377ae9c95ff752be78d4e99
Yaroslav Skorokhid <radomba@gmail.com>
2023-12-12 16:08:02 +0200

    Added feed item & feed loader

---
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,8 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Yaroslav Skorokhid on 12-12-2023.
+//
+
+import Foundation
---
+++ EssentialFeed/Feed Feature/FeedLoader.swift
@@ -0,0 +1,8 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Yaroslav Skorokhid on 12-12-2023.
+//
+
+import Foundation
