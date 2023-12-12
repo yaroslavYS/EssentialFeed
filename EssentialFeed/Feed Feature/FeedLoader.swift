93c5896ef864489a17ae008cf601b2cf478086c6
Yaroslav Skorokhid <radomba@gmail.com>
2023-12-12 16:11:48 +0200

    Push changes

--- EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/Feed Feature/FeedLoader.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
