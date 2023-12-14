be771a36be1d4388216e34a0fb165f42cf66329f
Yaroslav Skorokhid <radomba@gmail.com>
2023-12-14 17:35:24 +0200

    Swift 5 syntactic sugar = replaced reduce with compactMapValues

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -107,9 +107,7 @@ class RemoteFeedLoaderTests: XCTestCase {
             "description": description,
             "location": location,
             "image": imageURL.absoluteString
-        ].reduce(into: [String: Any]()) { (acc, e) in
-            if let value = e.value { acc[e.key] = value }
-        }
+        ].compactMapValues { $0 }
         return (item, json)
     }
     
