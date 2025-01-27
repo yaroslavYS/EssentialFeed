87a48579a1c7e96c25d7c9424e2bb6d047ccde20
Yaroslav Skorokhid <radomba@gmail.com>
2025-01-27 19:28:42 +0200

    Core Data Feed Store tests implementation

--- EssentialFeed/Feed API/CoreDataFeedStore.swift
+++ EssentialFeed/Feed API/CoreDataFeedStore.swift
@@ -38,7 +38,7 @@ public final class CoreDataFeedStore: FeedStore {
         let context = self.context
         context.perform {
             do {
-                let managedCache = ManagedCache(context: context)
+                let managedCache = try ManagedCache.newUniqueInstance(in: context)
                 managedCache.timestamp = timestamp
                 managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
 
@@ -51,7 +51,15 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        
+        let context = self.context
+        context.perform {
+            do {
+                try ManagedCache.find(in: context).map(context.delete).map(context.save)
+                completion(nil)
+            } catch {
+                completion(error)
+            }
+        }
     }
 }
 
@@ -95,6 +103,11 @@ private class ManagedCache: NSManagedObject {
         let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
         request.returnsObjectsAsFaults = false
         return try context.fetch(request).first
+    }
+    
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try find(in: context).map(context.delete)
+        return ManagedCache(context: context)
     }
     
     var localFeed: [LocalFeedImage] {
