ba96cc9aef24110ea5d79763967bbbca0931cca4
Yaroslav Skorokhid <radomba@gmail.com>
2025-02-25 17:27:36 +0200

    Add FeedLocalizationTests

--- EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
@@ -11,13 +11,6 @@ import EssentialFeediOS
 
 final class FeedUIIntegrationTests: XCTestCase {
     
-    func test_feedView_hasTitle() {
-        let (sut, _) = makeSUT()
-        
-        sut.loadViewIfNeeded()
-        XCTAssertEqual(sut.title, localized("FEED_VIEW_TITLE"))
-    }
-    
     func test_loadFeedActions_requestFeedFromLoader() {
         let (sut, loader) = makeSUT()
         
