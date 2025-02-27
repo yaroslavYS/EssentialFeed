a0d3c78f23b036b0269270e2693b16c717bac23a
Yaroslav Skorokhid <radomba@gmail.com>
2025-02-27 18:20:36 +0200

    Local feed image data loader implementation

--- EssentialFeed/Feed Cache/Infrastructure/Core Data/ManagedFeedImage.swift
+++ EssentialFeed/Feed Cache/Infrastructure/Core Data/ManagedFeedImage.swift
@@ -8,18 +8,25 @@
 import CoreData
 
 @objc(ManagedFeedImage)
-internal class ManagedFeedImage: NSManagedObject {
-    @NSManaged internal var id: UUID
-    @NSManaged internal var imageDescription: String?
-    @NSManaged internal var location: String?
-    @NSManaged internal var url: URL
-    @NSManaged internal var cache: ManagedCache
-    
+class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var data: Data?
+    @NSManaged var cache: ManagedCache
 }
 
 extension ManagedFeedImage {
+    static func first(with url: URL, in context: NSManagedObjectContext) throws -> ManagedFeedImage? {
+            let request = NSFetchRequest<ManagedFeedImage>(entityName: entity().name!)
+            request.predicate = NSPredicate(format: "%K = %@", argumentArray: [#keyPath(ManagedFeedImage.url), url])
+            request.returnsObjectsAsFaults = false
+            request.fetchLimit = 1
+            return try context.fetch(request).first
+        }
 
-    internal static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+    static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
         return NSOrderedSet(array: localFeed.map { local in
             let managed = ManagedFeedImage(context: context)
             managed.id = local.id
@@ -30,7 +37,7 @@ extension ManagedFeedImage {
         })
     }
 
-    internal var local: LocalFeedImage {
+    var local: LocalFeedImage {
         return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
     }
 }
